87c97867134f29cf964d9ed639a0c89a58c8f23b
Michelle <michelle.tu20@ycdsbk12.ca>
2019-06-16 13:45:00 -0400

    point counter works

--- finalproject.py
+++ finalproject.py
@@ -21,17 +21,11 @@ HEIGHT = 580
 # ------------   Global Variables   ------------
 
 
-# ---- Counters ----
-timer = 0
-points_counter = 0
-lost_ball = False
-
 # ---- Positions of Platforms And Balls ----
 player_x = WIDTH/2
 player_y = 50
 ball_x = random.randrange(12, WIDTH - 12)
 ball_y = random.randrange(HEIGHT - 100, HEIGHT - 12)
-# minimum height will be changed to the top of the platform
 
 # ---- Speed and Direction of Balls ----
 
@@ -39,32 +33,22 @@ ballmove_y = 2
 ballmove_x = random.randrange(-2, 4)
 if ballmove_x == 0 :
     ballmove_x += 2
-'''ballmove_y = random.randrange(-2, 6)'''
-
-'''
-ball2_x = random.randrange(10, WIDTH - 10)
-ball2_y = random.randrange(player_y, HEIGHT - 10)  # minimum height will be changed to the top of the platform
-'''
 
 # ---- Variables for the keys used in this program ----
 left_pressed = False
 right_pressed = False
 
+# ---- Points and Time Counters ----
+ball_paddle_collision = False
+
+points = 0
+time = 60
+
 
 # ------------   Main Game Code   ------------
 
-def on_draw():
-    global player_x, player_y, ball_x, ball_y, timer, YourScore
-    arcade.start_render()
-    # ---- Draw Commands Below ----
-    arcade.draw_rectangle_filled(player_x, player_y, 150, 15, arcade.color.WHITE)
-    arcade.draw_circle_filled(ball_x, ball_y, 12, arcade.color.WHITE)
-    #output = "Score: ".format(int(YourScore))
-    #arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
-
-
 def on_update(delta_time):
-    global right_pressed, left_pressed, player_x, player_y, ball_y, ball_x, timer, ballmove_x, ballmove_y
+    global right_pressed, left_pressed, player_x, player_y, ball_y, ball_x, timer, ballmove_x, ballmove_y, points
     if right_pressed:
         player_x += 8
     if left_pressed:
@@ -75,6 +59,8 @@ def on_update(delta_time):
         if ball_y == player_y + 12:
             if player_x - 75 <= ball_x <= player_x + 75:
                 ballmove_y *= -1
+                ball_paddle_collision is True
+                points += 1
 
             else: break
 
@@ -93,7 +79,14 @@ def on_update(delta_time):
         if ball_y > HEIGHT - 12:
             ballmove_y *= -1
 
-    # arcade.check_for_collision()
+
+def on_draw():
+    global player_x, player_y, ball_x, ball_y, points, ball_paddle_collision
+    arcade.start_render()
+    # ---- Draw Commands Below ----
+    arcade.draw_rectangle_filled(player_x, player_y, 150, 15, arcade.color.WHITE)
+    arcade.draw_circle_filled(ball_x, ball_y, 12, arcade.color.WHITE)
+    arcade.draw_text("Points: " + str(points), 20, 550, arcade.color.WHITE, 14)
 
 
 def on_key_press(key, modifiers):
