52d8ff0ac5bd3dd1b11f1701cc78d8639978663d
Michelle <michelle.tu20@ycdsbk12.ca>
2019-06-18 20:49:35 -0400

    everything done

--- finalproject.py
+++ finalproject.py
@@ -1,7 +1,8 @@
 '''
 -------------------------------------------------------------------------------
 Name:		finalproject.py
-Purpose:	<a description of your program>
+Purpose:	A game in which you try and keep the ball from falling
+            by bouncing it off a bar
 
 Author:	    Tu.M
 
@@ -21,47 +22,46 @@ HEIGHT = 580
 
 # ------------   Global Variables   ------------
 
+# ---- Miscellaneous Variables ----
 number_of_balls = 2
-game_over = False
-
+fail_message_x = 100
+fail_message = ""
 
 # ---- Positions of Platforms And Balls ----
-player_x = WIDTH/2
-player_y = 50
+bar_x = WIDTH/2
+bar_y = 50
 ball_x = random.randrange(12, WIDTH - 12)
 ball_y = HEIGHT - 12
 
 # ---- Speed and Direction of Balls ----
-
 ballmove_y = 2
 ballmove_x = random.randrange(-2, 4)
-if ballmove_x == 0 :
+if ballmove_x == 0:
     ballmove_x += 2
 
-# ---- Variables for the keys used in this program ----
+# ---- True-False Variables ----
 left_pressed = False
 right_pressed = False
 space_pressed = False
 ball_paddle_collision = True
+game_over = False
 
-fail_message = ""
 # ---- Counters ----
 points = 0
 time = 0
-
-
 
 # ------------   Main Game Code   ------------
 
 
 def on_update(delta_time, sound=None):
-    global right_pressed, left_pressed, player_x, player_y, ball_y, ball_x, timer, ballmove_x, ballmove_y, points, time, fail_message, number_of_balls, ball_paddle_collision
+    global right_pressed, left_pressed, bar_x, bar_y, ball_y, ball_x, time, ballmove_x, ballmove_y, points, time, \
+        fail_message, number_of_balls, ball_paddle_collision, fail_message_x
     if right_pressed:
-        if player_x+ 75 < WIDTH:
-            player_x += 10
+        if bar_x + 75 < WIDTH:
+            bar_x += 10
     if left_pressed:
-        if player_x - 75 > 0:
-            player_x -= 10
+        if bar_x - 75 > 0:
+            bar_x -= 10
 
     if space_pressed is True:
         if ball_paddle_collision is False:
@@ -69,21 +69,21 @@ def on_update(delta_time, sound=None):
 
     for _ in range(3):
 
-        if ball_y == player_y + 12:
-            if player_x - 75 <= ball_x <= player_x + 75:
+        if ball_y == bar_y + 12:
+            if bar_x - 75 <= ball_x <= bar_x + 75:
                 ballmove_y *= -1
                 ball_paddle_collision is True
                 points += 1
-        elif ball_y < player_y + 12:
+        elif ball_y < bar_y + 12:
             ball_paddle_collision = False
             ballmove_x = 0
             ballmove_y = 0
             if number_of_balls < 1:
                 fail_message = "Game Over"
+                fail_message_x = 250
                 game_over is True
             else:
                 fail_message = "Press Space Bar to Play Again"
-
 
         ball_x += ballmove_x
         ball_y += ballmove_y
@@ -102,26 +102,28 @@ def on_update(delta_time, sound=None):
 
     time += delta_time
 
+
 def on_draw():
-    global player_x, player_y, ball_x, ball_y, points, ball_paddle_collision, fail_message, number_of_balls
+    global bar_x, bar_y, ball_x, ball_y, points, ball_paddle_collision, fail_message, number_of_balls, fail_message_x
     arcade.start_render()
     # ---- Draw Commands Below ----
     # - Main Game Drawings -
-    arcade.draw_rectangle_filled(player_x, player_y, 150, 15, arcade.color.WHITE)
-    arcade.draw_circle_filled(ball_x, ball_y, 12, arcade.color.WHITE)
+    arcade.draw_rectangle_filled(bar_x, bar_y, 150, 15, arcade.color.WHITE)
+    arcade.draw_circle_filled(ball_x, ball_y, 12, arcade.color.BANANA_YELLOW)
     # - Points Counter Drawing -
-    arcade.draw_text("Points: " + str(points), 20, 550, arcade.color.WHITE, 14)
+    arcade.draw_text("Points: " + str(points), 20, 550, arcade.color.WHITE, 18)
     # - Timer Drawing -
     seconds = round(time % 100)
     timer = f"Time: {seconds}"
-    arcade.draw_text(timer, 620, 550, arcade.color.WHITE, 14)
-
-    arcade.draw_text(fail_message, 100, 300, arcade.color.WHITE, 30)
-    arcade.draw_text("Number of Balls Left: " + str(number_of_balls), 200, 550, arcade.color.WHITE, 14)
+    arcade.draw_text(timer, 590, 550, arcade.color.WHITE, 18)
+    # - Balls Left & Fail Message Drawings -
+    arcade.draw_text(fail_message,fail_message_x , 300, arcade.color.WHITE, 30)
+    arcade.draw_text("Balls Left: " + str(number_of_balls), 300, 550, arcade.color.WHITE, 18)
 
 
 def on_key_press(key, modifiers):
-    global right_pressed, left_pressed, space_pressed, number_of_balls, ball_paddle_collision, player_x, player_y,ball_x, ball_y,ballmove_x,ballmove_y,time,points,fail_message
+    global right_pressed, left_pressed, space_pressed, number_of_balls, ball_paddle_collision, bar_x, bar_y, \
+        ball_x, ball_y, ballmove_x, ballmove_y, time, points, fail_message
     if key == arcade.key.A:
         left_pressed = True
     if key == arcade.key.D:
@@ -130,13 +132,12 @@ def on_key_press(key, modifiers):
         if (ball_paddle_collision is False) and (number_of_balls >= 1):
             ball_paddle_collision is True
             number_of_balls -= 1
-            player_x = WIDTH / 2
-            player_y = 50
+            bar_x = WIDTH / 2
+            bar_y = 50
             ball_x = random.randrange(12, WIDTH - 12)
             ball_y = HEIGHT - 12
 
             # ---- Speed and Direction of Balls ----
-
             ballmove_y = 2
             ballmove_x = random.randrange(-2, 4)
             if ballmove_x == 0:
@@ -152,8 +153,6 @@ def on_key_press(key, modifiers):
             # ---- Counters ----
             points = 0
             time = 0
-        # space_pressed = True
-
 
 
 def on_key_release(key, modifiers):
@@ -164,10 +163,9 @@ def on_key_release(key, modifiers):
         right_pressed = False
 
 
-
 def setup():
     arcade.open_window(WIDTH, HEIGHT, "Bouncing Ball Game")
-    arcade.set_background_color(arcade.color.BLACK)
+    arcade.set_background_color(arcade.color.BLUEBONNET)
     arcade.schedule(on_update, 1/100)
 
     # Override arcade window methods
